1efb59266292bb4b95a5f46c32f5b979d7cff2e5
Geert Jansen <geertj@gmail.com>
2013-02-23 23:51:30 +0100

    Update copyright headers for 2013

--- lib/dbusx/test/test_message.py
+++ lib/dbusx/test/test_message.py
@@ -3,8 +3,8 @@
 # available under the terms of the MIT license. See the file "LICENSE" that
 # was provided together with this source file for the licensing terms.
 #
-# Copyright (c) 2012 the python-dbusx authors. See the file "AUTHORS" for a
-# complete list.
+# Copyright (c) 2012-2013 the python-dbusx authors. See the file "AUTHORS"
+# for a complete list.
 
 import six
 import math
